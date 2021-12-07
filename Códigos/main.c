c4322b47ef4b07888a0b14b16a7203fde4414be8
WarleyLeandro <warley.leandro95@gmail.com>
2021-12-07 15:26:32 -0300

    merge menu e funcoes

--- Códigos/main.c
+++ Códigos/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int insert(int key, char value, BD BD),
+    remove(int key, BD BD),
+    search(int key, BD BD),
+    update(int key, char value, BD BD);
+
 int main()
 {
+
+    BD myBD;
+    myBD.newRow.id = 1;
+    myBD.newRow.type = 'a';
+    myBD.newRow.data = 'b';
+
+    printf("%d", &myBD.newRow.id);
+
+    return 1;
+
     int opcao=1;
 
 
@@ -73,4 +88,62 @@ int main()
     }
 
     return 0;
+}
+
+   //TODO: transformar data em um array de string
+
+    typedef struct
+    {
+        int id;
+        char type;
+        char data;
+        
+    } Row;
+    
+    typedef struct
+    {
+        Row newRow;
+
+    } BD;
+    
+    
+
+
+int insert(int key, char value, BD BD) {
+
+    return 1;
+}
+
+int remove(int key, BD BD) {
+    for (int i = key; i < BD.newRow.id; i++)
+    {
+        if (key == BD.newRow.id)
+        {
+            BD.newRow.id = 0;
+            return 1;
+        }        
+    } 
+    return -1;
+}
+
+int search(int key, BD BD) {
+    for (int i = key; i < BD.newRow.id; i++)
+    {
+        if (key == BD.newRow.id)
+        {
+            return BD.newRow.data;
+        }        
+    }    
+    return -1;
+};
+
+int update(int key, char value, BD BD) {
+    for (int i = key; i < BD.newRow.id; i++)
+    {
+        if (key == BD.newRow.id)
+        {
+            BD.newRow.data = value;
+        }        
+    }    
+    return -1;
 }

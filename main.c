203590034187a88c9d2fc32a72f66a0f7a8f6817
Psmotta <55262114+Psmotta@users.noreply.github.com>
2021-12-06 22:13:54 -0300

    Código inicio

--- main.c
+++ main.c
@@ -1,78 +1,76 @@
-#include <stdio.h>;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int opcao=1;
 
 
-int insert(int key, char value, BD BD),
-    remove(int key, BD BD),
-    search(int key, BD BD),
-    update(int key, char value, BD BD);
-
-int main(void) {
-
-    BD myBD;
-    myBD.newRow.id = 1;
-    myBD.newRow.type = 'a';
-    myBD.newRow.data = 'b';
-
-    printf("%d", &myBD.newRow.id);
-
-    return 1;
-
-};
-
-    //TODO: transformar data em um array de string
-
-    typedef struct
+    while(opcao!=0)
     {
-        int id;
-        char type;
-        char data;
-        
-    } Row;
-    
-    typedef struct
-    {
-        Row newRow;
+        printf("**********OPCOES**********");
+        printf("\nEscolha uma das opcoes abaixo: ");
+        printf("\n1-Inserir\n2-Remover\n3-Buscar\n4-Atualizar\n0-Sair\nOpcao: ");
+        scanf("%d", &opcao);
 
-    } BD;
-    
-    
-
-
-int insert(int key, char value, BD BD) {
-
-    return 1;
-}
-
-int remove(int key, BD BD) {
-    for (int i = key; i < BD.newRow.id; i++)
-    {
-        if (key == BD.newRow.id)
+        switch(opcao)
         {
-            BD.newRow.id = 0;
-            return 1;
-        }        
-    } 
-    return -1;
-}
+        case 1:
 
-int search(int key, BD BD) {
-    for (int i = key; i < BD.newRow.id; i++)
-    {
-        if (key == BD.newRow.id)
-        {
-            return BD.newRow.data;
-        }        
-    }    
-    return -1;
-};
+            system("cls");
+            printf("Opcao Inserir\n");
+            system("pause");
+            system("cls");
 
-int update(int key, char value, BD BD) {
-    for (int i = key; i < BD.newRow.id; i++)
-    {
-        if (key == BD.newRow.id)
-        {
-            BD.newRow.data = value;
-        }        
-    }    
-    return -1;
+            break;
+
+        case 2:
+
+            system("cls");
+            printf("Opcao Remover\n");
+            system("pause");
+            system("cls");
+
+            break;
+
+        case 3:
+
+            system("cls");
+            printf("Opcao Buscar\n");
+            system("pause");
+            system("cls");
+
+            break;
+
+        case 4:
+
+            system("cls");
+            printf("Opcao Atualizar\n");
+            system("pause");
+            system("cls");
+
+            break;
+
+        case 0:
+
+            printf("\nSaindo do programa!\n");
+            system("pause");
+            system("cls");
+
+            break;
+
+        default:
+
+            system("cls");
+            printf("\nDigite uma opcao valida!\n");
+            system("pause");
+            system("cls");
+
+            break;
+
+        }
+
+    }
+
+    return 0;
 }
